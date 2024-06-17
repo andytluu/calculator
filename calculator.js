990cd3ad5f74bb35003fa1d250ba66409ec6c7b2
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-06-16 18:17:36 -0700

    Added operator functions

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+console.log(add(1,2));
+console.log(subtract(2,1));
+console.log(multiply(2,2));
+console.log(divide(4,2));
