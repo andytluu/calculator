5677ea3de83b00aba5f285c0965687033058b0f4
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-06-18 11:46:54 -0700

    Added events for operations and equal

--- calculator.js
+++ calculator.js
@@ -15,25 +15,30 @@ function divide(a, b){
 }
 
 function operate(fNum, sNum, oper){
+    let result = 0;
     switch(oper){
         case "+":
-            add(fNum, sNum);
+            result = add(fNum, sNum);
             break;
         case "-":
-            subtract(fNum, sNum);
+            result = subtract(fNum, sNum);
             break;
         case "x":
-            multiply(fNum, sNum);
+            result = multiply(fNum, sNum);
             break;
         case "/":
-            divide(fNum, sNum);
+            result = divide(fNum, sNum);
             break;
     }
+
+    return result;
 }
 
 const numBtns = document.querySelectorAll(".nums");
 const display = document.querySelector("#display");
 const clear = document.querySelector("#del");
+const oper = document.querySelectorAll(".operate");
+const equal = document.querySelector("#equal");
 let firstNum = 0;
 let secondNum = 0;
 let operator = "";
@@ -41,6 +46,31 @@ let store = 0;
 
 clear.addEventListener("click", () => {
     display.textContent = "";
+    firstNum = 0;
+    secondNum = 0;
+
+    console.log(firstNum);
+    console.log(secondNum);
+});
+equal.addEventListener("click", () => {
+    secondNum = parseInt(display.textContent);
+    store = operate(firstNum,secondNum,operator);
+    display.textContent = store.toString();
+    firstNum = 0;
+    secondNum = 0;
+
+    console.log(result);
+});
+oper.forEach((elem) => {
+    elem.addEventListener("click", () => {
+        operator = elem.textContent;
+        firstNum = parseInt(display.textContent);
+        display.textContent = "";
+
+
+        console.log(operator);
+        console.log(firstNum);
+    });
 });
 numBtns.forEach((elem) => {
     elem.addEventListener("click", () => {
@@ -48,7 +78,3 @@ numBtns.forEach((elem) => {
         display.append(txt);
     });
 });
-console.log(add(1,2));
-console.log(subtract(2,1));
-console.log(multiply(2,2));
-console.log(divide(4,2));
