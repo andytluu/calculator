d8ad0a5a430062d311db185d5145d29a10330fb5
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-06-18 15:56:44 -0700

    Fixed display and operation event

--- calculator.js
+++ calculator.js
@@ -39,15 +39,15 @@ const display = document.querySelector("#display");
 const clear = document.querySelector("#del");
 const oper = document.querySelectorAll(".operate");
 const equal = document.querySelector("#equal");
-let firstNum = 0;
-let secondNum = 0;
+let firstNum = undefined;
+let secondNum = undefined;
 let operator = "";
 let store = 0;
 
 clear.addEventListener("click", () => {
     display.textContent = "";
-    firstNum = 0;
-    secondNum = 0;
+    firstNum = undefined;
+    secondNum = undefined;
 
     console.log(firstNum);
     console.log(secondNum);
@@ -56,17 +56,28 @@ equal.addEventListener("click", () => {
     secondNum = parseInt(display.textContent);
     store = operate(firstNum,secondNum,operator);
     display.textContent = store.toString();
-    firstNum = 0;
-    secondNum = 0;
+    firstNum = undefined;
+    secondNum = undefined;
 
     console.log(result);
 });
 oper.forEach((elem) => {
     elem.addEventListener("click", () => {
-        operator = elem.textContent;
-        firstNum = parseInt(display.textContent);
-        display.textContent = "";
 
+        if(firstNum === undefined){
+            operator = elem.textContent;
+            firstNum = parseInt(display.textContent);
+        }
+        else{
+            secondNum = parseInt(display.textContent);
+            store = operate(firstNum,secondNum,operator);
+            display.textContent = store.toString();
+            firstNum = store;
+            operator = elem.textContent;
+            secondNum = undefined;
+        }
+
+        
 
         console.log(operator);
         console.log(firstNum);
@@ -76,5 +87,9 @@ numBtns.forEach((elem) => {
     elem.addEventListener("click", () => {
         let txt = elem.textContent;
         display.append(txt);
+        if(firstNum !== undefined && operator !== "" && secondNum == undefined){
+            secondNum = parseInt(display.textContent);
+            display.textContent = txt;
+        }
     });
 });
